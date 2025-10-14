77cfeb6e83526ad12da591970ec7a1e1ebf06805
Rohit Parit <rohitparit70@gmail.com>
2025-10-14 19:32:09 +0530

    This is program of calculator

---
+++ 3.py
@@ -0,0 +1,8 @@
+# Simple Calculator
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
